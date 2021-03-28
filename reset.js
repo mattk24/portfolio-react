9e5329db2e2d9d9354c98f48ae3005c684ddab65
Matthew Koukios <48831464+mattk24@users.noreply.github.com>
2021-03-28 17:33:06 -0400

    Delete reset.js

--- reset.js
+++
@@ -1,6 +0,0 @@
-let btnClear = document.querySelector('button');
-let inputs = document.querySelector('input');
-
-btnClear.addEventListener('click', () => {
-    inputs.forEach(input => input.value = '');
-});
